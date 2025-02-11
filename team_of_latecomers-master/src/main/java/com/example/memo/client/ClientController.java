5dfa4ce51c623ea5776cab60b62418fb76f795b2
maralina13 <112951167+maralina13@users.noreply.github.com>
2025-02-12 02:44:41 +0300

    Delete team_of_latecomers-master directory

--- team_of_latecomers-master/src/main/java/com/example/memo/client/ClientController.java
+++
@@ -1,6 +0,0 @@
-package com.example.memo.client;
-
-public class ClientController {
-    // Здесь можно реализовать логику обработки действий пользователя,
-    // связывая UI и сетевой слой.
-}
--- team_of_latecomers-master/src/main/java/com/example/memo/client/IGameClientUI.java
+++
@@ -1,8 +0,0 @@
-package com.example.memo.client;
-
-import com.example.memo.server.GameState;
-
-public interface IGameClientUI {
-    void updateGameState(GameState state);
-    void showGameResult(GameState finalState);
-}
